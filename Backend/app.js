f300e55aea2299a6d83c408fc4829027c01a96f5
vedika sharma <sharmavedika765@gmail.com>
2026-05-19 20:22:34 +0530

    Fixed CORS for production

--- Backend/app.js
+++ Backend/app.js
@@ -29,8 +29,11 @@ mongoose.connect(dbUrl)
 
 // ================= MIDDLEWARE =================
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://wanderlust-red-six.vercel.app"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
